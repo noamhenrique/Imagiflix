fc5dd2440a83f4f9df7d20fd21d71fbd950813bf
Noam Henrique <noamhenrique@gmail.com>
2023-05-10 21:16:48 -0300

    feat(Navbar): search button with hover created

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,10 @@ import logo from "../assets/imagiflix.png";
 import { faSearch, faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import placeholderUser from "../assets/user.jpg";
 
+import "./Navbar.css";
+
 const Navbar = () => (
-  <nav className="absolute top-0 left-0 grid grid-cols-2 items-center w-full p-8">
+  <nav className="navbar absolute top-0 left-0 grid grid-cols-2 items-center w-full p-8">
     <div className="justify-self-start grid grid-cols-2 gap-4 items-center">
       <h1 className="hidden">Imagiflix</h1>
       <img src={logo} alt="Imagiflix" />
@@ -21,13 +23,16 @@ const Navbar = () => (
     </div>
 
     <div className="justify-self-end flex justify-items-end items-center">
-      <form className="relative">
+      <form className="relative w-64">
         <input
-          className="w-full bg-black"
+          className="w-full bg-black border border-white rounded py-1 px-3 transition-all duration-300 ease-in-out opacity-0 hover:opacity-100 focus:opacity-100"
           type="text"
           placeholder="Titulos, gente e gênero"
         />
-        <button className="absolute r-2">
+        <button
+          className="absolute right-2 py-1"
+          onClick={(e) => e.preventDefault()}
+        >
           <FontAwesomeIcon icon={faSearch} />
         </button>
       </form>
