604b0ce67ca1defc49ba9621dca453cd49d32c59
Noam Henrique <noamhenrique@gmail.com>
2023-05-29 18:45:49 -0300

    User button created and stylized

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import logo from "../assets/imagiflix.png";
@@ -6,52 +7,72 @@ import placeholderUser from "../assets/user.jpg";
 
 import "./Navbar.css";
 
-const Navbar = () => (
-  <nav className="navbar absolute top-0 left-0 grid grid-cols-2 items-center w-full p-8">
-    <div className="justify-self-start grid grid-cols-2 gap-4 items-center">
-      <h1 className="hidden">Imagiflix</h1>
-      <img src={logo} alt="Imagiflix" />
-      <ul className="grid grid-flow-col gap-4">
-        <li className="font-bold">Início</li>
-        <li>
-          <a href="#series">Séries</a>
-        </li>
-        <li>
-          <a href="#movies">Filmes</a>
-        </li>
-      </ul>
-    </div>
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    <div className="justify-self-end flex justify-items-end items-center">
-      <form className="relative w-64">
-        <input
-          className="w-full bg-black border border-white rounded py-1 px-3 transition-all duration-300 ease-in-out opacity-0 hover:opacity-100 focus:opacity-100"
-          type="text"
-          placeholder="Titulos, gente e gênero"
-        />
-        <button
-          className="absolute right-2 py-1"
-          onClick={(e) => e.preventDefault()}
-        >
-          <FontAwesomeIcon icon={faSearch} />
-        </button>
-      </form>
-      <div className="flex ml-4">
-        <img src={placeholderUser} alt="Foto do usuário" />
-        <button>
-          <FontAwesomeIcon className="ml-2" icon={faCaretDown} />
-        </button>
-        <ul className="hidden">
+  const handleClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className="navbar absolute top-0 left-0 grid grid-cols-2 items-center w-full p-8">
+      <div className="justify-self-start grid grid-cols-2 gap-4 items-center">
+        <h1 className="hidden">Imagiflix</h1>
+        <img src={logo} alt="Imagiflix" />
+        <ul className="grid grid-flow-col gap-4">
+          <li className="font-bold">Início</li>
           <li>
-            <a href="#account">Minha conta</a>
+            <a href="#series">Séries</a>
           </li>
           <li>
-            <a href="#logout">Sair</a>
+            <a href="#movies">Filmes</a>
           </li>
         </ul>
       </div>
-    </div>
-  </nav>
-);
+
+      <div className="justify-self-end flex justify-items-end items-center">
+        <form className="relative w-64">
+          <input
+            className="w-full bg-black border border-white rounded py-1 px-3 transition-all duration-300 ease-in-out opacity-0 hover:opacity-100 focus:opacity-100"
+            type="text"
+            placeholder="Titulos, gente e gênero"
+          />
+          <button
+            className="absolute right-2 py-1"
+            onClick={(e) => e.preventDefault()}
+          >
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </form>
+        <div className="relative flex ml-4">
+          <img
+            src={placeholderUser}
+            alt="Foto do usuário"
+            onClick={handleClick}
+          />
+          <button onClick={handleClick}>
+            <FontAwesomeIcon className="ml-2" icon={faCaretDown} />
+          </button>
+          <ul
+            className={`absolute mt-10 top-0 right-0 w-32 bg-black rounded py-1 px-3 transition-all duration-500 ease-in-out ${
+              !isMenuOpen && "opacity-0 invisible"
+            }`}
+          >
+            <li>
+              <a href="#account" onClick={handleClick}>
+                Minha conta
+              </a>
+            </li>
+            <li>
+              <a href="#logout" onClick={handleClick}>
+                Sair
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
