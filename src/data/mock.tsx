137aedf60ac624d36ea5917dc5cad60b41aad631
Noam Henrique <noamhenrique@gmail.com>
2023-06-14 15:46:53 -0300

    feat(Carousel): working with interface and typing

--- src/data/mock.tsx
+++ src/data/mock.tsx
@@ -2,7 +2,13 @@ import cover1 from "../assets/movie1.jpg";
 import cover2 from "../assets/movie2.jpg";
 import cover3 from "../assets/movie3.jpg";
 
-const mockData = [
+export interface Movie {
+  title: string;
+  cover: string;
+  score?: number | string;
+}
+
+const mockData: Movie[] = [
   {
     title: "Avengers",
     cover: cover1,
