f26739a0a1f222135d08a27259adf78893802897
Noam Henrique <noamhenrique@gmail.com>
2023-06-14 21:54:33 -0300

    wip(Carousel): adjusting the arrows button

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,4 +1,9 @@
 import Slick from "react-slick";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 import mockData, { Movie } from "../data/mock";
 
@@ -13,20 +18,33 @@ interface CarouselData {
   data?: Movie[];
 }
 
-const Carousel = ({
-  title = "Filmes em destaque",
-  data = mockData,
-}: CarouselData) => {
+const Carousel = ({ title = "Carousel", data = mockData }: CarouselData) => {
+  enum Direction {
+    left,
+    right,
+  }
+  const SlickArrow = ({ direction }: { direction: Direction }) => (
+    <button
+      className={`absolute w-16 h-full z-10 bg-red-500 ${
+        direction ? "right-0" : "left-0"
+      }`}
+    >
+      <FontAwesomeIcon icon={direction ? faChevronRight : faChevronLeft} />
+    </button>
+  );
+
   const options = {
     infinite: true,
     slidesToScroll: 1,
     variableWidth: true,
+    prevArrow: <SlickArrow direction={Direction.left} />,
+    nextArrow: <SlickArrow direction={Direction.right} />,
   };
 
   return (
     <section>
       <h2>{title}</h2>
-      <Slick {...options}>
+      <Slick className="relative" {...options}>
         {data.map((movie, index) => Poster(movie, index))}
       </Slick>
     </section>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,32 +15,31 @@ const Hero = ({ title = "Avengers Endgame", score = 10 }) => {
   };
 
   return (
-    <header className="relative min-h-screen">
+    <header className="relative min-h-screen -mb-64">
       <img
         className="object-cover object-center h-full w-full"
         src={placeholder}
         alt="Filme em destaque"
       />
-      {
-        <article className="absolute bottom-0 mb-64 px-8">
-          <p className="text-2xl">Assista agora:</p>
-          <h2 className="text-5xl font-bold">{title}</h2>
-          <p className="text-base">
-            Nota{" "}
-            <span
-              className={`inline-block mx-2 my-6 py-2 px-3 border-4 bg-slate-950/75 rounded-full ${getBorderColor()}`}
-            >
-              {score}
-            </span>
-          </p>
-          <button className="text-base py-2 px-8 mr-2 rounded bg-slate-950/50 transition-all duration-300 ease-in-out hover:bg-slate-50/75 hover:text-black">
-            <FontAwesomeIcon className="mr-2" icon={faPlay} /> Assistir
-          </button>
-          <button className="text-base py-2 px-8 mr-2 rounded bg-slate-950/50 transition-all duration-300 ease-in-out hover:bg-slate-50/75 hover:text-black">
-            <FontAwesomeIcon className="mr-2" icon={faPlus} /> Minha Lista
-          </button>
-        </article>
-      }
+      <div className="absolute left-0 bottom-0 w-full h-full bg-gradient-to-b from-transparent to-black"></div>
+      <article className="absolute bottom-0 mb-64 pb-16 px-8">
+        <p className="text-2xl">Assista agora:</p>
+        <h2 className="text-5xl font-bold">{title}</h2>
+        <p className="text-base">
+          Nota{" "}
+          <span
+            className={`inline-block mx-2 my-6 py-2 px-3 border-4 bg-slate-950/75 rounded-full ${getBorderColor()}`}
+          >
+            {score}
+          </span>
+        </p>
+        <button className="text-base py-2 px-8 mr-2 rounded bg-slate-950/50 transition-all duration-300 ease-in-out hover:bg-slate-50/75 hover:text-black">
+          <FontAwesomeIcon className="mr-2" icon={faPlay} /> Assistir
+        </button>
+        <button className="text-base py-2 px-8 mr-2 rounded bg-slate-950/50 transition-all duration-300 ease-in-out hover:bg-slate-50/75 hover:text-black">
+          <FontAwesomeIcon className="mr-2" icon={faPlus} /> Minha Lista
+        </button>
+      </article>
     </header>
   );
 };
