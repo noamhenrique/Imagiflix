23e35db51791839261e8aacb7729b5e8ba534b4d
Noam Henrique <noamhenrique@gmail.com>
2023-06-16 16:10:10 -0300

    feat(Carousel): hover and css details adjusted

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -9,6 +9,8 @@ import mockData, { Movie } from "../data/mock";
 
 import Poster from "./Poster";
 
+import "./Carousel.css";
+
 interface CarouselData {
   title?: string;
   data?: Movie[];
@@ -49,8 +51,8 @@ const Carousel = ({ title = "Carousel", data = mockData }: CarouselData) => {
   };
 
   return (
-    <section>
-      <h2 className="relative z-10 font-bold text-2xl ml-8 mb-4">{title}</h2>
+    <section className="carousel">
+      <h2 className="relative z-10 font-bold text-2xl ml-8">{title}</h2>
       <Slick className="relative mb-8" {...options}>
         {data.map((movie, index) => Poster(movie, index))}
       </Slick>
--- src/components/Poster.tsx
+++ src/components/Poster.tsx
@@ -3,13 +3,15 @@ import { faPlayCircle } from "@fortawesome/free-regular-svg-icons";
 
 import { Movie } from "../data/mock";
 
+import "./Poster.css";
+
 const Poster = ({ cover, title, score }: Movie, index: number) => (
   <article
-    className="relative transition-all duration-500 ease-in-out hover:scale-110"
+    className="relative transition-all duration-500 ease-in-out hover:scale-110 hover:z-10"
     key={index}
   >
     <img src={cover} alt={title} />
-    <div className="absolute inset-0 w-full h-full grid place-items-center bg-black bg-opacity-75 transition-all duration-500 ease-in-out">
+    <div className="poster absolute inset-0 w-full h-full grid place-items-center bg-black bg-opacity-75 transition-all duration-500 ease-in-out opacity-0">
       <FontAwesomeIcon icon={faPlayCircle} size="5x" />
       <h2 className="text-2xl">{title}</h2>
     </div>
