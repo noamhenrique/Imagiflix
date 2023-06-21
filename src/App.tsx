7660bacb7bb24521fc6ef48b12392e96599cba21
Noam Henrique <noamhenrique@gmail.com>
2023-06-20 21:07:44 -0300

    feat(API): Starting the implementation of the API from themoviedb

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Carousel from "./components/Carousel";
@@ -6,6 +8,31 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const App = () => {
+  const URL = "https://api.themoviedb.org/3";
+  const IMAGEURL = "https://image.tmdb.org/t/p";
+  const APIKEY = "2f6069067672601762aebc5e306ac4fd";
+  const APISPRING = `?api_key=${APIKEY}&language=pt-BR`;
+
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `${URL}/discover/movie${APISPRING}&sort_by=popularity.desc`
+      );
+
+      const data = await response.json();
+
+      setMovies(data);
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    movies && console.log(movies);
+  }, [movies]);
+
   return (
     <div className="m-auto antialised font-sans bg-black text-white">
       <Hero />
